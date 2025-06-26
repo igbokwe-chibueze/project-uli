bb2f3894b0b36d0db56905da19bd3975221b6883
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-06-26 22:04:39 +0100

    Updated theme-toggle.tsx to allow for system selection
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,21 +1,39 @@
 // src/components/theme-toggle.tsx
-import React from 'react'
-import { Button } from '@/components/ui/button'
+
 import { MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const ThemeToggle = () => {
-    const { setTheme, theme } = useTheme()
+    const { setTheme } = useTheme()
   return (
-    <>
-        <Button
-            variant="ghost"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-            <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-        </Button>
-    </>
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+                <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
